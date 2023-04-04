96b0f8e444dd11e133c716624c8a5f49a766a436
Jerald-lyk <72423471+Jerald-lyk@users.noreply.github.com>
2023-04-05 00:52:59 +0800

    clean up

---
+++ Scraper/fb1.py
@@ -0,0 +1,50 @@
+"""
+Download comments for a public Facebook post.
+"""
+import facebook_scraper as fs
+import csv
+import boto3
+from io import BytesIO
+import time
+import schedule
+import datetime
+import pandas as pd
+
+# get POST_ID from the URL of the post which can have the following structure:
+# https://www.facebook.com/USER/posts/POST_ID
+# https://www.facebook.com/groups/GROUP_ID/posts/POST_ID
+# https://www.facebook.com/sghealthministry/posts/take-your-booster-as-soon-as-you-are-eligible-here-are-some-answers-to-guide-you/317010660454364/
+POST_ID = "317010660454364"
+
+# number of comments to download -- set this to True to download all comments
+MAX_COMMENTS = 200
+
+# get the post (this gives a generator)
+gen = fs.get_posts(
+    post_urls=[POST_ID],
+    options={"comments": MAX_COMMENTS, "progress": True}
+)
+
+# take 1st element of the generator which is the post we requested
+post = next(gen)
+
+# extract the comments part
+comments = post['comments_full']
+
+df = pd.DataFrame.from_dict(comments)
+df.drop(labels=['comment_id', 'comment_url', 'commenter_id','commenter_url','commenter_meta','comment_image',\
+                      'comment_reactors','comment_reactions', 'replies'], axis='columns', inplace=True)
+df.rename(columns={'commenter_name': 'author', 'comment_text': 'body', 'comment_reaction_count': 'score', 'comment_time': 'date'}, inplace=True)
+df['source'] = 'fb'
+df['score'].fillna('0', inplace=True)
+df['date'] = pd.to_datetime(df['date'])
+df['date'] = df['date'].dt.strftime('%B %Y')
+df.to_json("fb1.json", orient='records')
+
+with open('fb1.json', 'rb') as file:
+    json_file = BytesIO(file.read())
+
+s3 = boto3.resource('s3')
+bucket_name = 'is459-g1t8-project'  # replace this with your S3 bucket name
+object_key = 'read/fb1.json'  # the key under which the object will be stored in the S3 bucket
+s3.Bucket(bucket_name).upload_fileobj(json_file, object_key)
---
+++ Scraper/fb2.py
@@ -0,0 +1,69 @@
+"""
+Download comments for a public Facebook post.
+"""
+import facebook_scraper as fs
+import csv
+import boto3
+from io import BytesIO
+import time
+import schedule
+import datetime
+import pandas as pd
+
+# get POST_ID from the URL of the post which can have the following structure:
+# https://www.facebook.com/USER/posts/POST_ID
+# https://www.facebook.com/groups/GROUP_ID/posts/POST_ID
+# https://www.facebook.com/sghealthministry/posts/are-you-armed-with-a-booster-shot-yet-from-14-feb-2022-a-booster-shot-is-necessa/320010360154394/
+# def download_and_upload_comments():
+#     today = datetime.datetime.today()
+#     if today.day == 31:
+        
+POST_ID = "320010360154394"
+
+# number of comments to download -- set this to True to download all comments
+MAX_COMMENTS = 100
+
+# get the post (this gives a generator)
+gen = fs.get_posts(
+    post_urls=[POST_ID],
+    options={"comments": MAX_COMMENTS, "progress": True},
+    credentials=('is459g1t8@gmail.com' ,"qwer1234!")
+)
+
+# take 1st element of the generator which is the post we requested
+post = next(gen)
+
+# extract the comments part
+comments = post['comments_full']
+
+# process comments as you want...
+
+df = pd.DataFrame.from_dict(comments)
+df.drop(labels=['comment_id', 'comment_url', 'commenter_id','commenter_url','commenter_meta','comment_image',\
+                      'comment_reactors','comment_reactions', 'replies'], axis='columns', inplace=True)
+df.rename(columns={'commenter_name': 'author', 'comment_text': 'body', 'comment_reaction_count': 'score', 'comment_time': 'date'}, inplace=True)
+df['source'] = 'fb'
+df['score'].fillna('0', inplace=True)
+df['date'] = pd.to_datetime(df['date'])
+df['date'] = df['date'].dt.strftime('%B %Y')
+df.to_json("fb2.json", orient='records')
+
+with open('fb2.json', 'rb') as file:
+    json_file = BytesIO(file.read())
+
+s3 = boto3.resource('s3')
+bucket_name = 'is459-g1t8-project'  # replace this with your S3 bucket name
+object_key = 'read/fb2.json'  # the key under which the object will be stored in the S3 bucket
+s3.Bucket(bucket_name).upload_fileobj(json_file, object_key)
+
+# schedule the job to run once every day at a specific time
+# schedule.every().day.at('12:00').do(download_and_upload_comments)
+
+# # run the scheduled job
+# while True:
+#     schedule.run_pending()
+#     time.sleep(1)
+
+#     # e.g. ...get the replies for them
+#     for reply in comment['replies']:
+#         print(' ', reply)
